7954fed6d5e4e3473e7d8c106e4187327a7cfcd4
Lucas Adriano <lukasadriano94@hotmail.com>
2025-12-05 14:05:10 -0300

    Inicializando estudos dart - feitos até 05/12/2025

---
+++ android/app/src/main/kotlin/com/lucasadriano/trilhaapp/trilhaapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.lucasadriano.trilhaapp.trilhaapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        useMaterial3: false, //coloquei pq o material3 desatualizado com o curso
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var numeroGerado = 0;
+
+  int _gerarNumeroAleatorio() {
+    Random numeroAleatorio = Random();
+    return numeroGerado = numeroAleatorio.nextInt(1000);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    debugPrint("Chamando método build");
+    return Scaffold(
+      appBar: AppBar(title: const Text("Meu App")),
+      body: Center(child: Text(numeroGerado.toString())),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add_box),
+        onPressed: () {
+          setState(() {
+            numeroGerado = _gerarNumeroAleatorio();
+          });
+        },
+      ),
+    );
+  }
+}
+// iniciar video de "Organizando o Projeto"
